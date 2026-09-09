3d27470dbe701d35724de5c6a96300f3f50244a9
lekhana-07 <hanabhoj07@gmail.com>
2026-09-09 15:52:33 +0530

    EXP 2.3

---
+++ minmax_p.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void minMax(int a[], int n, int *min, int *max)
+{
+    *min = a[0];
+    *max = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < *min)
+            *min = a[i];
+
+        if (a[i] > *max)
+            *max = a[i];
+    }
+}
+
+int main()
+{
+    int data[] = {7, 2, 9, 4, 1};
+
+    int min, max;
+
+    minMax(data, 5, &min, &max);
+
+    cout << "Minimum = " << min << endl;
+    cout << "Maximum = " << max << endl;
+
+    return 0;
+}
